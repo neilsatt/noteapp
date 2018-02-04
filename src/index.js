8f7bcdfdbe379916014367344cbc183b26a73ce1
neilsatt <neilsatt@gmail.com>
2018-02-03 19:15:51 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Board from './Board'
+
+
+ReactDOM.render(<Board count={7}/>, document.getElementById('root'))
